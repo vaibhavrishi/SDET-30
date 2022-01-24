0997c184a3d20edd7b4688c3c8779bfe3167ca97
User <User@DESKTOP-P309ICG>
2022-01-24 22:29:57 +0530

    user1 first commit

---
+++ src/test/java/github/DemoGitHub.java
@@ -0,0 +1,15 @@
+package github;
+
+import org.testng.annotations.Test;
+
+public class DemoGitHub {
+	
+	@Test
+	
+	public void denoGithubTest() {
+		
+		System.out.println("user 1 first commit");
+	}
+	
+
+}
